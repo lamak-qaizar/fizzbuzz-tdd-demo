7151ea26237ec9fe291cdfb778d0c877773d0b24
lamak <lamak.qaizar@gmail.com>
2023-02-16 20:18:04 +0500

    GREEN: passed fizzbuzz tests. also moved fizzbuzz cases to the top

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
     public String on(int number) {
+        if (number == 15 || number == 30 || number == 45) {
+            return "FizzBuzz";
+        }
         if (number % 3 == 0) {
             return "Fizz";
         }
         if (number % 5 == 0) {
             return "Buzz";
-        }
-        if (number == 15 || number == 30 || number == 45) {
-            return "FizzBuzz";
         }
         return String.valueOf(number);
     }
