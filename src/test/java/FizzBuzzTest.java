f82e06e8ca5ec7ca04e5ece55a6f0caaeb051077
lamak <lamak.qaizar@gmail.com>
2023-02-16 20:03:09 +0500

    GREEN: passed test of not multiple of 3 nor 5

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,8 +4,8 @@ public class FizzBuzzTest {
 
     @Test
     public void shouldReturnNumberWhenNotAMultipleOf3or5() {
-        assert new FizzBuzz().on(1) == "1";
-        assert new FizzBuzz().on(1) == "2";
-        assert new FizzBuzz().on(1) == "4";
+        assert new FizzBuzz().on(1).equals("1");
+        assert new FizzBuzz().on(2).equals("2");
+        assert new FizzBuzz().on(4).equals("4");
     }
 }
