87202e4d9a503040830c0e81f9da976d9206f363
lamak <lamak.qaizar@gmail.com>
2023-02-16 20:07:29 +0500

    REFACTOR: generalizaed fizz implementation

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public String on(int number) {
-        if (number == 3 || number == 6 || number == 9) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
         return String.valueOf(number);
