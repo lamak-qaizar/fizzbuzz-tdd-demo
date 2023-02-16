80673a0e3b94c1aff8c600e26ea016da8971bbd9
lamak <lamak.qaizar@gmail.com>
2023-02-16 20:02:01 +0500

    RED: added more cases for not multiple of 3 nor 5 so that implementation can be generalized

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,5 +5,7 @@ public class FizzBuzzTest {
     @Test
     public void shouldReturnNumberWhenNotAMultipleOf3or5() {
         assert new FizzBuzz().on(1) == "1";
+        assert new FizzBuzz().on(1) == "2";
+        assert new FizzBuzz().on(1) == "4";
     }
 }
