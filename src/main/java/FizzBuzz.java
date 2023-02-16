ad1bdfd9e4e9d9c30ecd9c7fdd7a02795e71177a
lamak <lamak.qaizar@gmail.com>
2023-02-16 20:06:41 +0500

    GREEN: passed fizz test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public String on(int number) {
-        if (number == 3) {
+        if (number == 3 || number == 6 || number == 9) {
             return "Fizz";
         }
         return String.valueOf(number);
