dc216a0c29c14856271083e6c1a80de990e5feb9
lamak <lamak.qaizar@gmail.com>
2023-02-16 20:01:15 +0500

    REFACTOR: improved test name

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void test() {
+    public void shouldReturnNumberWhenNotAMultipleOf3or5() {
         assert new FizzBuzz().on(1) == "1";
     }
 }
